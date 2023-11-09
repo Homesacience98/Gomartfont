b18d8b1d4767d3537eba0f9393f3ab878c573f3d
Homesacience98 <itssajj@gmail.com>
2023-11-09 15:06:43 +0100

    deployment vercel

---
+++ src/components/User/ProfileForm.js
@@ -0,0 +1,125 @@
+import React, { useState, useEffect } from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+function ProfileForm() {
+  const [customerData, setCustomerData] = useState({
+    customeruser: '', // Initialize with an empty string
+    phone_number: '',
+    city: '',
+    customerimage: null, // Use 'null' if no image is selected
+  });
+
+  useEffect(() => {
+    // Retrieve the logged-in user from local storage when the component mounts
+    const user = localStorage.getItem('loggedInUsername') || '';
+    setCustomerData({ ...customerData, customeruser: user });
+
+    try {
+      const existingData = JSON.parse(localStorage.getItem('lastCustomer'));
+      if (existingData) {
+        setCustomerData(existingData);
+      }
+    } catch (error) {
+      console.error('Error parsing existing data:', error);
+    }
+  }, []);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCustomerData({
+      ...customerData,
+      [name]: value,
+    });
+  };
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setCustomerData({
+      ...customerData,
+      customerimage: file,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('customeruser', customerData.customeruser);
+    formData.append('phone_number', customerData.phone_number);
+    formData.append('city', customerData.city);
+    formData.append('customerimage', customerData.customerimage);
+
+    const response = await fetch('http://itssaj.pythonanywhere.com/api/add-customer/', {
+      method: 'POST',
+      body: formData,
+    });
+
+    if (response.ok) {
+      window.location.reload();
+      window.open('/profile-view');
+    } else {
+      console.error('Update failed with status:', response.status);
+    }
+  };
+
+  return (
+    <div className="container mt-4">
+      <hr style={{ color: 'lime' }} />
+      <div style={{ backgroundColor: '#097969', color: 'white' }}>
+        &nbsp; <h6> &nbsp; Profile Settings <i class="fas fa-marker"></i></h6>
+        <hr />
+      </div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="phone_number" className="form-label">
+            <i class='fas fa-mobile-alt'></i> Phone Number:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="phone_number"
+            value={customerData.phone_number}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="city" className="form-label">
+            <i class='fas fa-map-marker-alt'></i> Location:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="city"
+            value={customerData.city}
+            onChange={handleInputChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="customerimage" className="form-label">
+            <i class='fas fa-images'></i> Profile Picture:
+          </label>
+          <input
+            type="file"
+            className="form-control"
+            name="customerimage"
+            onChange={handleImageChange}
+          />
+        </div>
+
+        <button
+          type="submit"
+          style={{ float: '', textDecoration: '', color: 'lime' }}
+          className="btn btn-dark"
+        >
+          <i class='fas fa-user-cog'></i> Update Profile
+        </button>
+      </form>
+      <br/>
+    </div>
+  );
+}
+
+export default ProfileForm;
---
+++ src/components/User/Profileview.js
@@ -0,0 +1,149 @@
+import React, { useState, useEffect } from 'react';
+import Navbar3 from '../Navbar/Navbar3';
+import { Link } from 'react-router-dom';
+import { MDBCollapse, MDBBtn } from 'mdb-react-ui-kit';
+import CartUser from '../Cart/CartUser';
+import ProfileForm from './ProfileForm';
+
+function ViewProfile() {
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showShow, setShowShow] = useState(false);
+  const [showShow2, setShowShow2] = useState(false);
+
+  const toggleShow = () => setShowShow(!showShow);
+  const toggleShow2 = () => setShowShow2(!showShow2);
+
+  useEffect(() => {
+    // Fetch the list of customers from your Django backend
+    fetch('http://itssaj.pythonanywhere.com/api/customer-list/') // Replace with your API endpoint
+      .then((response) => response.json())
+      .then((data) => {
+        setCustomers(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching customer list:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  const loggedInUser = localStorage.getItem('loggedInUsername');
+  const filteredCustomers = customers.filter((customer) => customer.customeruser === loggedInUser);
+  const reversedCustomers = filteredCustomers.slice().reverse();
+  const lastCustomer = reversedCustomers[0];
+  // Save the lastCustomer to local storage
+  localStorage.setItem('lastCustomer', JSON.stringify(lastCustomer));
+
+  return (
+    <div>
+      <div className="video-container">
+        <iframe
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/6sIBZCFyz14"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+      <hr style={{ color: 'lime' }} />
+
+      {loading ? (
+        <p>{loggedInUser} profile...</p>
+      ) : lastCustomer ? (
+        <div class='' style={{backgroundColor:''}}>
+          <div class='container' style={{backgroundColor:'white', borderRadius: '7px', border: 'lime 2px solid', }}>
+            <br/>
+                  <h4       style={{float:'right'}}
+ ><b> <i class='fab fa-expeditedssl'></i> </b></h4>
+            <img
+              src={`http://itssaj.pythonanywhere.com${lastCustomer.customerimage}`}
+              alt="Profile"
+              style={{ maxWidth: '200px', border: 'white 3px solid', borderRadius: '10px',marginLeft:'15px',marginTop:'-10px' }}
+            />
+{/* <i class='fas fa-images'></i> */}
+          <div class=''>
+            <Link tag="a" onClick={toggleShow2} style={{ float: 'right', textDecoration: 'none', color: 'green', marginTop: '-19px' }}>
+              <span style={{ color: 'green' }}>
+                <i className="fas fa-marker"></i> Edit Profile
+              </span>
+            </Link>
+            <i class='fas fa-images'></i>
+
+          </div>
+          <hr style={{ color: 'green' }} />
+
+
+          <MDBCollapse show={showShow2}>
+            <ProfileForm />
+          </MDBCollapse>
+
+          </div>
+
+<hr/>
+          <div style={{ backgroundColor: '#191970', border: 'solid white 2px', color: 'white' }}>
+            <br />
+            <p>
+              <strong>
+                &nbsp; <i class="fas fa-user-injured"></i>{' '}
+              </strong>
+              <b> {lastCustomer.customeruser} </b>
+            </p>
+          </div>
+
+          <hr style={{ color: 'pink' }} />
+
+          <div class='container' style={{backgroundColor:'#F0F0F0', borderRadius: '7px' , border: 'solid black 1px'}}>
+
+<br/>
+          <p style={{ float: '' }}>
+            <strong>
+              {' '}
+              <i style={{ color: 'red' }} class="fab fa-centercode"></i> Verified Gomart Profile{' '}
+            </strong>{' '}
+            <i style={{ color: 'red' }} class="fas fa-check-circle"></i>{' '}
+          </p>
+
+          <hr style={{ color: '' }} />
+
+          <p style={{ float: '', color: 'grey' }}>
+            <strong> <i class="fas fa-phone"></i> Phone Number: &nbsp;&nbsp; </strong>
+            {lastCustomer.phone_number}
+          </p>
+
+          <p style={{ float: '', color: 'grey' }}>
+            <strong> <i class="fas fa-map-marker-alt"></i> &nbsp; Location: </strong>
+            {lastCustomer.city}
+          </p>
+          <br/>
+          </div>
+
+        </div>
+      ) : (
+        <p>No customer profile found for the logged-in user.</p>
+      )}
+
+<div class='container' style={{backgroundColor:'white', borderRadius: '3px' }}>
+  <br/>
+        <Link tag="a" onClick={toggleShow} style={{ float: 'right', textDecoration: 'none', color: '#088F8F' }}>
+          <i class="fas fa-cart-arrow-down"></i>
+          <span style={{ color: '#088F8F' }}> Cart</span>
+        </Link>
+
+        <Link to="/checkout" style={{ float: '', textDecoration: 'none', color: 'brown' }}>
+          <i class="fab fa-cc-amazon-pay"></i> &nbsp; Checkout
+        </Link>
+<hr/>
+      </div>
+      <MDBCollapse show={showShow}>
+          <CartUser />
+        </MDBCollapse>
+
+      <hr style={{ color: 'pink' }} />
+      <br /> <br /> <br />
+    </div>
+  );
+}
+
+export default ViewProfile;
